9aabfdefd0206fac2b9652a7b1700c89e587c60e
yiliu <yil349@ucsd.edu>
2025-03-10 15:48:39 -0700

    fix: add batch dim in flux pipeline

--- src/diffusers/models/transformers/transformer_flux.py
+++ src/diffusers/models/transformers/transformer_flux.py
@@ -458,16 +458,10 @@ class FluxTransformer2DModel(
         encoder_hidden_states = self.context_embedder(encoder_hidden_states)
 
         if txt_ids.ndim == 3:
-            logger.warning(
-                "Passing `txt_ids` 3d torch.Tensor is deprecated."
-                "Please remove the batch dimension and pass it as a 2d torch Tensor"
-            )
+            assert txt_ids.shape[0] == 1
             txt_ids = txt_ids[0]
         if img_ids.ndim == 3:
-            logger.warning(
-                "Passing `img_ids` 3d torch.Tensor is deprecated."
-                "Please remove the batch dimension and pass it as a 2d torch Tensor"
-            )
+            assert img_ids.shape[0] == 1
             img_ids = img_ids[0]
 
         ids = torch.cat((txt_ids, img_ids), dim=0)
